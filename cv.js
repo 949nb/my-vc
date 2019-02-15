72c70d2829cb2085c94d3cfa9fd5c53203df32fe
hanyu949 <hy949@zhanghacBookPro.lan>
2019-02-15 15:39:14 +0800

    顶部菜单栏效果

--- cv.js
+++ cv.js
@@ -3,4 +3,11 @@ setTimeout(function(){
 },2000
     )
 //loading页面计时器···
+window.onscroll = function(){
+    if(scrollY>0){
+        topBar.classList.add("active")
+    }else{
+        topBar.classList.remove("active")
+    }
+}
 
