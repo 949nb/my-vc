5eedc2812d99295ccf5a5d976e8a90c26ab90007
hanyu949 <hy949@zhanghacBookPro.lan>
2019-02-17 00:04:21 +0800

    添加了跳转功能

--- cv.js
+++ cv.js
@@ -22,3 +22,16 @@ subMenu.onmouseleave = function(){
     submenu.classList.remove("active")
 }
 
+var aTags = document.querySelectorAll(".top-nav-bar > ul > li > a")
+
+for(var i = 0; i < aTags.length; i++){
+    aTags[i].onclick = function(x){
+        x.preventDefault()
+        let a = x.currentTarget//把点击到的元素赋给a
+        var href = a.getAttribute('href')//获取点击到的元素的href，也就是a的href
+        console.log(href)
+        var element = document.querySelector(href)
+        var top = element.offsetTop
+        window.scrollTo(0,top - 80)
+    }
+}
