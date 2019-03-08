418015df4952fcdf0005abe4949659c8e8488dae
hanyu949 <hy949@hanyu94cBookPro.lan>
2019-03-09 01:40:35 +0800

    获取留言板信息

--- massage.js
+++ massage.js
@@ -8,6 +8,19 @@ appKey: APP_KEY
 
 var myForm = document.querySelector('#postMessageForm')
 
+//获取留言板数据
+var query = new AV.Query('Message');
+  query.find().then(function (messages) {
+    for(i=0;i<messages.length;i++){
+    console.log(messages[i].attributes.content)
+    }
+  }).then(function() {
+    //alert('获取留言信息成功～')
+  }, function (error) {
+      //更新失败
+  });
+
+//提交留言板数据
 myForm.addEventListener('submit',function(e){
     e.preventDefault()
     var content = myForm.querySelector('input[name=content]').value
